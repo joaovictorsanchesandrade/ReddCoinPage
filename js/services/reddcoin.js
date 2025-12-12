1a73a78ef59b4d35356d9d515b2ef005df322f6a
João Victor Sanches Andrade <joaovictorsanchesandradeestuda@gmail.com>
2025-12-12 20:11:18 -0300

    Function in service reddcoin fetch UTXOs for multiple addreesses.

--- js/services/reddcoin.js
+++ js/services/reddcoin.js
@@ -1,24 +1,27 @@
 /*
     All functions that communicate with the API are declared here
     there are no business rules, just pure REST communication.
+
+    Note: The documentation is in English because I chose a
+    language that everyone understands, but that's more of a native Brazilian language.
 */
 
 /**
  * Retrieve the data from the provided address.
  * @param {{ address: string }} params - Object with the address.
  * @returns {Promise<{
- *  addrStr: string, 
- *  balance: number, 
- *  balanceSat: number
- *  totalReceived: number,
- *  totalReceivedSat: number,
- *  totalSent: number,
- *  totalSentSat: number,
- *  unconfirmedBalance: number,
- *  unconfirmedBalanceSat: number,
- *  unconfirmedTxApperances: number,
- *  txApperances: number,
- *  transactions: string[]
+ * addrStr: string, 
+ * balance: number, 
+ * balanceSat: number
+ * totalReceived: number,
+ * totalReceivedSat: number,
+ * totalSent: number,
+ * totalSentSat: number,
+ * unconfirmedBalance: number,
+ * unconfirmedBalanceSat: number,
+ * unconfirmedTxApperances: number,
+ * txApperances: number,
+ * transactions: string[]
  * }>} Full address details
 */
 async function fetchAddr({ address }) {
@@ -32,4 +35,29 @@ async function fetchAddr({ address }) {
     return response.json()
 }
 
-export default reddcoin = {fetchAddr};
+/**
+ * Search for UTXOs for multiple addresses.
+ * @param {{addresses: string[]}} params - List of public addresses
+ * @returns {Promise<{
+ * address: string,
+ * txid: string,
+ * vout: number,
+ * ts: number,
+ * scriptPubKey: string,
+ * amount: number,
+ * confirmations: number,
+ * confirmationsFromCache: boolean
+ * }[]>} All UXTOs from the provided addresses
+ */
+async function fetchUxtos({ addresses }) {
+    // Consulting the UXTOs of the addresses
+    const response = await fetch(`https://live.reddcoin.com/api/addrs/${addresses.join(',')}/utxo`)
+
+    // Validating the response
+    if (!response.ok){
+        throw Error('It was not possible to obtain the UXTOs for these wallets.')
+    }
+    return response.json()
+}
+
+export default reddcoin = {fetchAddr, fetchUxtos};
